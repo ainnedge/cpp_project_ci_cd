b6fd52f07654ef0c97014b60af6301bd4fa4cf05
ainnedge <ainnedge@gmail.com>
2022-07-22 16:46:59 +0530

    added multest3 as test case

--- test/cpp_project_ci_cdTest.cpp
+++ test/cpp_project_ci_cdTest.cpp
@@ -18,3 +18,11 @@ TEST(GtestTester, mulTest2) {
   EXPECT_EQ( result, n1 * n2 );
 }
 
+TEST(GtestTester, mulTest3) {
+  const int n1 = 7;
+  const int n2 = 20;
+  const int result = cppProjectCiCd::mul(n1, n2);
+  
+  EXPECT_EQ( result, n1 * n2 );
+}
+
